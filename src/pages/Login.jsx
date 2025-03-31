da65c9517d8ad9ec75ac194383c16d39ddbd93ac
klu230033153 <2300033153@kluniversity.in>
2025-03-31 19:12:30 +0530

    Created Login Page, and releated components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Header from "./../components/Header";
+import Header from "../components/Header";
+import LoginPageComponent from "../components/LoginPageComponent";
+import "../styles/login.css";
 
 const Login = () => {
   return (
-    <div>
+    <div id="login">
       <Header />
-      Login
+      <LoginPageComponent />
     </div>
   );
 };
